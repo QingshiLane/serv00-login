a5e08f2c0ccc6aad89c169d6fe02d0459b882f9c
QingshiLane <98565268+QingshiLane@users.noreply.github.com>
2024-08-12 17:26:26 +0800

    Update login_script.py

--- login_script.py
+++ login_script.py
@@ -90,7 +90,7 @@ async def main():
             now_utc = format_to_iso(datetime.utcnow())
             now_beijing = format_to_iso(datetime.utcnow() + timedelta(hours=8))
             success_message = f'{serviceName}账号 {username} 于北京时间 {now_beijing}（UTC时间 {now_utc}）登录成功！'
-            message += success_message + '\n'
+            message += '\n' + success_message + '\n'
             print(success_message)
         else:
             message += f'{serviceName}账号 {username} 登录失败，请检查{serviceName}账号和密码是否正确。\n'
