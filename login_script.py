d1a01074cac23c4bb061513fc1b334bc06686f46
QingshiLane <98565268+QingshiLane@users.noreply.github.com>
2024-08-12 18:21:49 +0800

    Update login_script.py

--- login_script.py
+++ login_script.py
@@ -69,7 +69,7 @@ async def login(username, password, panel):
 
 async def main():
     global message
-    message = 'serv00&ct8自动化脚本运行\n'
+    message = 'serv00&ct8自动化脚本运行:\n\n'
 
     try:
         async with aiofiles.open('accounts.json', mode='r', encoding='utf-8') as f:
@@ -95,11 +95,11 @@ async def main():
         if is_logged_in:
             now_utc = format_to_iso(datetime.utcnow())
             now_beijing = format_to_iso(datetime.utcnow() + timedelta(hours=8))
-            success_message = f'{serviceName}账号 {username} 北京时间 {now_beijing}（UTC时间 {now_utc}）登录成功！'
-            message += '  ' +success_message + '\n'
+            success_message = f'{serviceName}账号_{username}_于CST:{now_beijing}(UTC:{now_utc})登录成功！'
+            message += success_message + '\n'
             print(success_message)
         else:
-            message += f'\n{serviceName}账号 {username} 登录失败，请检查{serviceName}账号和密码是否正确。\n'
+            message += f'\n{serviceName}账号_{username}_登录失败，请检查{serviceName}账号和密码是否正确。\n\n'
             print(f'{serviceName}账号 {username} 登录失败，请检查{serviceName}账号和密码是否正确。')
 
         delay = random.randint(1000, 8000)
