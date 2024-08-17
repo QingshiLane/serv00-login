249944abe7fc2101911c81115713425038a3c179
QingshiLane <98565268+QingshiLane@users.noreply.github.com>
2024-08-17 16:06:37 +0800

    Update login_script.py

--- login_script.py
+++ login_script.py
@@ -98,7 +98,7 @@ async def main():
             ss+=1
             now_utc = format_to_iso(datetime.utcnow())
             now_beijing = format_to_iso(datetime.utcnow() + timedelta(hours=8))
-            success_message = f'🟢{serviceName} 账号 <code>{username}</code> <small>CST {now_beijing}（UTC {now_utc}）</small>'
+            success_message = f'🟢{serviceName} 账号 <code>{username}</code> CST <small>{now_beijing}</small>（UTC <small>{now_utc}</small>）'
             message += success_message + '\n'
             print(success_message)
         else:
